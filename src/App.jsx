b9bcadf0b64fb807010b4080d2742cef769c8030
Dahvid <dahvidweb@gmail.com>
2025-11-27 07:00:31 -0800

    Edit to toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./index.css";
+import "./toast.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
